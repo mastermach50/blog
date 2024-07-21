3aedad48501de2e5b3034d16bc099ca54c6982c3
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2024-07-21 16:48:44 +0530

    added og images

--- md2html.py
+++ md2html.py
@@ -1,19 +1,27 @@
 import os
 
+def variable_replacement(file):
+    with open(file, "r") as f:
+        contents = f.read()
+    
+    with open(file, "w") as f:
+        contents = contents.replace("{{{filename_without_ext}}}", filename_without_ext)
+        f.write(contents)
+
+
 # Get list of articles
 md_files = os.listdir("./articles/raw")
 
 # Convert articles from markdown to html using pandoc
 for file in md_files:
     filename_without_ext = file.replace(".md", "")
-    title = filename_without_ext.replace("-", " ").title()
-
-
     os.system(f"pandoc -f markdown -t html --columns 100 --template template.article.html ./articles/raw/{file} -o ./articles/{filename_without_ext}.html")
-
+    variable_replacement(f"./articles/{filename_without_ext}.html")
     print(f"Processed {filename_without_ext}")
 
-
 # Convert index from markdown to html
+filename_without_ext = "index"
 os.system(f"pandoc -f markdown -t html --columns 100 --template template.index.html ./index.md -o ./index.html")
+variable_replacement(f"./index.html")
+
 print("Processed Index")
